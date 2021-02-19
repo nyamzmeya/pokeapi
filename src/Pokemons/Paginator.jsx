0789b19a79979c3586c6f8705945458f9697c981
Anna <annazmei2014@gmail.com>
2021-02-20 00:59:15 +0300

    second commit

--- src/Pokemons/Paginator.jsx
+++ src/Pokemons/Paginator.jsx
@@ -3,18 +3,30 @@ import 'antd/dist/antd.css';
 import s from './Pokemons.module.css';
 
 const Paginator = (props) => {
+    
 
     let onChange = page => {
         props.onPageChange(page)
     };
 
-    return <Pagination current={props.currentPage} 
+    let state = { matches: window.matchMedia("(max-width: 575px)").matches };
+
+    return <>
+    {state.matches && (<Pagination simple defaultCurrent={props.currentPage} 
     total={200} 
     defaultPageSize={10}
     onChange= {onChange}
     showSizeChanger= {false}
     className= {s.paginator}
-    />
+    />)}
+    {!state.matches && (<Pagination current={props.currentPage} 
+    total={200} 
+    defaultPageSize={10}
+    onChange= {onChange}
+    showSizeChanger= {false}
+    className= {s.paginator}
+    />)}
+    </>
     
 }
 
